00ecfb3bca44ee1e5079797a931f86c25cab4a02
Srikanth K <122169588+py-code314@users.noreply.github.com>
2024-10-14 10:32:08 -0400

    feat: Add <dialog> & show it when button is clicked

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+const addBook = document.querySelector('.add-book');
+const closeButton = document.querySelector('.close-btn');
+const dialog = document.querySelector('#form-dialog');
+const bookName = document.querySelector('.title');
+const authorName = document.querySelector('.author');
+const confirmBtn = document.querySelector('.confirm-btn');
+const cancelBtn = document.querySelector('.cancel-btn');
+
+addBook.addEventListener('click', () => {
+  // show form
+  dialog.showModal();
+})
+
+
 
