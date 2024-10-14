c5733612a04b52b66a800026bc22ee56934e9d54
Srikanth K <122169588+py-code314@users.noreply.github.com>
2024-10-14 16:18:13 -0400

    feat: Create newBook with form input values

--- main.js
+++ main.js
@@ -1,28 +1,65 @@
 const addBook = document.querySelector('.library-heading__btn');
 const closeBtn = document.querySelector('.dialog--close-btn');
 const dialog = document.querySelector('.dialog');
+const form = document.querySelector('.dialog-form');
 const bookName = document.querySelector('.title');
 const authorName = document.querySelector('.author');
-const confirmBtn = document.querySelector('.confirm-btn');
-const cancelBtn = document.querySelector('.cancel-btn');
+const confirmBtn = document.querySelector('.dialog--confirm-btn');
+const cancelBtn = document.querySelector('.dialog--cancel-btn');
 
 addBook.addEventListener('click', () => {
+  // make sure fields are empty
+  if (bookName.value !== '' && authorName.value !== '') {
+    bookName.value = '';
+    authorName.value = '';
+  }
   // show form
   dialog.showModal();
 })
 
-dialog.addEventListener('close', (event) => {
-  if (dialog.returnValue === 'close') {
-    dialog.close();
-    
-  }
-  if (dialog.returnValue === 'cancel') {
-    event.preventDefault();
-  } else if (dialog.returnValue === 'confirm') {
-    event.preventDefault();
-    addBookToLibrary();
+cancelBtn.addEventListener('click', () => {
+  dialog.close();
+})
+
+closeBtn.addEventListener('click', () => {
+  dialog.close();
+})
+
+dialog.addEventListener('close', () => {
+  console.log(dialog.returnValue);
+  
+
+  if (dialog.returnValue === 'confirm') {
+    // create new formData object
+    const formData = new FormData(form);
+    // convert formData to object
+    const bookData = Object.fromEntries(formData);
+    console.log(bookData);
+    addBookToLibrary(bookData);
   }
 })
+
+const myLibrary = [];
+function Book(title, author) {
+  // the constructor...
+  this.title = title;
+  this.author = author;
+  // this.pages = pages;
+  // this.yearPublished = yearPublished;
+  // this.genre = genre;
+  // this.readStatus = readStatus;
+} 
+
+function addBookToLibrary(bookData) {
+  const newBook = new Book(bookData.title, bookData.author);
+  console.log(newBook);
+  myLibrary.push(newBook);
+
+  console.log(myLibrary);
+}
+  
+
+
 
 
 
