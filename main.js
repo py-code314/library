e25b2ff6fc5b8b40ae87158bb2d78c9905e4f231
Srikanth K <122169588+py-code314@users.noreply.github.com>
2024-10-16 13:36:53 -0400

    feat: Show pages-read field when readStatus is 'reading'

--- main.js
+++ main.js
@@ -1,68 +1,41 @@
 const addBook = document.querySelector('.library-heading__btn');
-const closeBtn = document.querySelector('.dialog--close-btn');
+
+
 const dialog = document.querySelector('.dialog');
 const form = document.querySelector('.dialog-form');
+
+const closeBtn = document.querySelector('.dialog--close-btn');
+
 const bookName = document.querySelector('.title');
 const authorName = document.querySelector('.author');
-const confirmBtn = document.querySelector('.dialog--confirm-btn');
+const percentage = document.querySelector('.dialog__percentage');
+
 const cancelBtn = document.querySelector('.dialog--cancel-btn');
+const confirmBtn = document.querySelector('.dialog--confirm-btn');
+
 const book = document.querySelector('.book');
 const bookButtons = document.querySelectorAll('.book__buttons');
+
 const readBtn = document.querySelector('.book__read-btn');
 const deleteBtn = document.querySelector('.book__delete-btn');
 
-// load books when page loads
-window.addEventListener('DOMContentLoaded', () => {
-  showBooks();
-});
-
-addBook.addEventListener('click', () => {
-  // make sure fields are empty
-  // if (bookName.value !== '' && authorName.value !== '') {
-  //   bookName.value = '';
-  //   authorName.value = '';
-  // }
-  // clear all form fields
-  form.reset();
-
-  // show form
-  dialog.showModal();
-});
-
-// TODO: add code for Esc key
-
-cancelBtn.addEventListener('click', () => {
-  // return 'cancel' to the dialog so that it doesn't send 'confirm'
-  dialog.close('cancel');
-});
-
-closeBtn.addEventListener('click', () => {
-  // return 'close' to the dialog so that it doesn't send 'confirm'
-  dialog.close('close');
-});
-
-dialog.addEventListener('close', (event) => {
-  event.preventDefault();
-  console.log(dialog.returnValue);
-
-  if (dialog.returnValue === 'confirm') {
-    // create new formData object
-    const formData = new FormData(form);
-    // convert formData to object
-    const bookData = Object.fromEntries(formData);
-    // console.log(bookData);
-    addBookToLibrary(bookData);
-    // showBooks();
-
-    // remove all divs with class book
-    const books = document.querySelectorAll('.book');
-    books.forEach((book) => book.remove());
-    // show all books in library
-    showBooks();
-  }
-});
-
 const myLibrary = [];
+const bookCovers = [
+  './images/book-covers/book.jpeg',
+  './images/book-covers/coffee.jpeg',
+  './images/book-covers/colors.jpeg',
+  './images/book-covers/dark-green.jpeg',
+  './images/book-covers/flowers.jpeg',
+  './images/book-covers/leaves.jpeg',
+  './images/book-covers/paper.jpeg',
+  './images/book-covers/red.jpeg',
+  './images/book-covers/roses.jpeg',
+  './images/book-covers/sky.jpeg',
+  './images/book-covers/stars.jpeg',
+  './images/book-covers/vase.jpeg',
+];
+
+// Book constructor
 function Book(
   title,
   author,
@@ -72,7 +45,6 @@ function Book(
   genre,
   readStatus
 ) {
-  // the constructor...
   this.title = title;
   this.author = author;
   this.coverImage = coverImage;
@@ -82,6 +54,7 @@ function Book(
   this.readStatus = readStatus;
 }
 
+// add books manually
 const dracula = new Book(
   'Dracula',
   'Bram Stoker',
@@ -92,6 +65,7 @@ const dracula = new Book(
   'reading'
 );
 myLibrary.push(dracula);
+
 const mother = new Book(
   'Mother',
   'Maxim Gorky',
@@ -102,6 +76,7 @@ const mother = new Book(
   'reading'
 );
 myLibrary.push(mother);
+
 const mockingBird = new Book(
   'To Kill a Mockingbird',
   'Harper Lee',
@@ -123,6 +98,7 @@ const hound = new Book(
   'yes'
 );
 myLibrary.push(hound);
+
 const fahrenheit = new Book(
   'Fahrenheit 451',
   'Ray Bradbury',
@@ -133,6 +109,7 @@ const fahrenheit = new Book(
   'no'
 );
 myLibrary.push(fahrenheit);
+
 const frankenstein = new Book(
   'Frankenstein',
   'Mary Shelley',
@@ -144,101 +121,63 @@ const frankenstein = new Book(
 );
 myLibrary.push(frankenstein);
 
-const bookCovers = [
-  './images/book-covers/book.jpeg',
-  './images/book-covers/coffee.jpeg',
-  './images/book-covers/colors.jpeg',
-  './images/book-covers/dark-green.jpeg',
-  './images/book-covers/flowers.jpeg',
-  './images/book-covers/leaves.jpeg',
-  './images/book-covers/paper.jpeg',
-  './images/book-covers/red.jpeg',
-  './images/book-covers/roses.jpeg',
-  './images/book-covers/sky.jpeg',
-  './images/book-covers/stars.jpeg',
-  './images/book-covers/vase.jpeg',
-];
+// load books when page loads
+window.addEventListener('DOMContentLoaded', () => {
+  showBooks();
+});
 
-function showBooks() {
-  myLibrary.forEach((book) => {
-    const bookDiv = document.createElement('div');
-    bookDiv.className = 'book';
-    bookDiv.dataset.index = myLibrary.indexOf(book);
+addBook.addEventListener('click', () => {
+  // make sure fields are empty
+  form.reset();
 
-    const bookContainer = document.createElement('div');
-    bookContainer.className = 'book__container';
+  // show form
+  dialog.showModal();
+  
+});
 
-    const coverDiv = document.createElement('div');
-    coverDiv.className = 'book__cover';
-
-    const coverPhoto = document.createElement('img');
-    // coverPhoto.src = book.coverImage;
-    // Add book covers from an array for books added by user
-    if (book.coverImage) {
-      coverPhoto.src = book.coverImage;
-    } else {
-      coverPhoto.src =
-        bookCovers[Math.floor(Math.random() * bookCovers.length)];
-    }
-
-    const detailsDiv = document.createElement('div');
-    detailsDiv.className = 'book__details';
-
-    const titleElement = document.createElement('h3');
-    titleElement.textContent = book.title;
-
-    const authorElement = document.createElement('p');
-    authorElement.textContent = book.author;
-
-    detailsDiv.append(titleElement, authorElement);
-
-    coverDiv.append(coverPhoto);
-    bookContainer.append(coverDiv, detailsDiv);
-    
-
-    const buttonsDiv = document.createElement('div');
-    buttonsDiv.className = 'book__buttons';
-
-    const readBtn = document.createElement('button');
-    readBtn.className = 'book__read-btn';
-    readBtn.innerHTML = `Read <img src="./images/update.svg" alt="update" /> `;
-    buttonsDiv.append(readBtn);
-
-    const deleteBtn = document.createElement('button');
-    deleteBtn.className = 'book__delete-btn';
-    deleteBtn.textContent = 'Delete';
-    deleteBtn.innerHTML = `Delete <img src="./images/delete.svg" alt="delete" /> `;
-
-    buttonsDiv.append(readBtn, deleteBtn);
-    // document.querySelector('.library').append(bookDiv);
-    if (book.readStatus === 'reading') {
-      document.querySelector('.reading').append(bookDiv);
-    } else {
-      document.querySelector('.library').append(bookDiv);
-    }
-
-    bookDiv.append(bookContainer, buttonsDiv);
-  });
-}
-
-document.addEventListener('click', (event) => {
-  if (event.target.classList.contains('book__delete-btn')) {
-    const bookDiv = event.target.closest('.book');
-    const bookIndex = Number(bookDiv.dataset.index);
-    myLibrary.splice(bookIndex, 1);
-    bookDiv.remove();
-    // console.log(myLibrary);
+// show percentage input only if readStatus is 'reading'
+dialog.addEventListener('input', (event) => {
+  // how to access these from global scope?
+  const readStatus = event.target.form.querySelector('#read-status').value;
+  const percentage = event.target.form.querySelector('.dialog__percentage');
+  if (readStatus === 'reading') {
+    percentage.style.display = 'flex';
+  } else {
+    percentage.style.display = 'none';
   }
 });
-// document.querySelectorAll('.book').forEach((book) => {
-//   book.addEventListener('mouseover', () => {
-//     bookButtons.style.display = 'block';
-//   });
 
-//   book.addEventListener('mouseout', () => {
-//     bookButtons.style.display = 'none';
-//   });
-// });
+// TODO: add code for Esc key
+
+closeBtn.addEventListener('click', () => {
+  // return 'close' to the dialog so that it doesn't send 'confirm'
+  dialog.close('close');
+});
+
+cancelBtn.addEventListener('click', () => {
+  // return 'cancel' to the dialog so that it doesn't send 'confirm'
+  dialog.close('cancel');
+});
+
+dialog.addEventListener('close', (event) => {
+  event.preventDefault();
+  console.log(dialog.returnValue);
+
+  if (dialog.returnValue === 'confirm') {
+    // create new formData object
+    const formData = new FormData(form);
+    // convert formData to object
+    const bookData = Object.fromEntries(formData);
+    console.log(bookData);
+    addBookToLibrary(bookData);
+    
+    // remove all divs with class book
+    const books = document.querySelectorAll('.book');
+    books.forEach((book) => book.remove());
+    // show all books in library
+    showBooks();
+  }
+});
 
 function addBookToLibrary(bookData) {
   const newBook = new Book(
@@ -250,10 +189,98 @@ function addBookToLibrary(bookData) {
     bookData.genre,
     bookData.readStatus
   );
-  // console.log(newBook);
+  console.log(newBook);
   myLibrary.push(newBook);
 
   console.log(myLibrary);
 }
+
+
+
+function showBooks() {
+  myLibrary.forEach((book) => {
+    // create book card
+    const bookDiv = document.createElement('div');
+    bookDiv.className = 'book';
+    // add index to book
+    bookDiv.dataset.index = myLibrary.indexOf(book);
+
+    // create book container
+    const bookContainer = document.createElement('div');
+    bookContainer.className = 'book__container';
+
+    // create cover div
+    const coverDiv = document.createElement('div');
+    coverDiv.className = 'book__cover';
+
+    const coverPhoto = document.createElement('img');
+    // Add book covers from an array for books added by user
+    if (book.coverImage) {
+      coverPhoto.src = book.coverImage;
+    } else {
+      coverPhoto.src =
+        bookCovers[Math.floor(Math.random() * bookCovers.length)];
+    }
+
+    // create details div
+    const detailsDiv = document.createElement('div');
+    detailsDiv.className = 'book__details';
+
+    const titleElement = document.createElement('h3');
+    titleElement.textContent = book.title;
+
+    const authorElement = document.createElement('p');
+    authorElement.textContent = book.author;
+
+    // create percentage div
+    const percentageDiv = document.createElement('div');
+    percentageDiv.className = 'percentage';
+    percentageDiv.textContent = `${book.readStatus}%`;
+
+    // append elements
+    detailsDiv.append(titleElement, authorElement);
+    coverDiv.append(coverPhoto);
+    bookContainer.append(coverDiv, detailsDiv);
+
+    // create buttons div
+    const buttonsDiv = document.createElement('div');
+    buttonsDiv.className = 'book__buttons';
+
+    // create buttons
+    const readBtn = document.createElement('button');
+    readBtn.className = 'book__read-btn';
+    readBtn.innerHTML = `Read <img src="./images/update.svg" alt="update" /> `;
+    // buttonsDiv.append(readBtn);
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.className = 'book__delete-btn';
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.innerHTML = `Delete <img src="./images/delete.svg" alt="delete" /> `;
+
+    // append buttons
+    buttonsDiv.append(readBtn, deleteBtn);
+
+    // append to book card
+    bookDiv.append(bookContainer, buttonsDiv);
+
+    // add book to library or continue reading section
+    // console.log(book.readStatus);
+    if (book.readStatus === 'reading') {
+      document.querySelector('.reading').append(bookDiv);
+    } else {
+      document.querySelector('.library').append(bookDiv);
+    }
+  });
+}
+
+// delete book
+document.addEventListener('click', (event) => {
+  if (event.target.classList.contains('book__delete-btn')) {
+    const bookDiv = event.target.closest('.book');
+    const bookIndex = Number(bookDiv.dataset.index);
+    myLibrary.splice(bookIndex, 1);
+    bookDiv.remove()
+  }
+});
 
 
